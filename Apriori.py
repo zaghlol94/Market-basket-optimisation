6fc9e32b6f563206bfbe77b147559143eea13f66
zaghlol94 <zaghlollight@gmail.com>
2017-09-10 14:22:17 +0200

    Update Apriori.py

--- Apriori.py
+++ Apriori.py
@@ -19,6 +19,6 @@ for i in range(dataset.shape[0]):
     transactions.append(li)  
 #training Apriori 
 from apyori import apriori
-rules=apriori(transactions,min_support=0.003,min_confidence=0.2,min_lift=3,min_lenth=2)    
+rules=apriori(transactions,min_support=0.003,min_confidence=0.2,min_lift=3,min_length=2)    
 
 res=list(rules)
